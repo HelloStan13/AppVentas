ecfdeeb0b8280adb8edb0d146ee4bf00292fc6bd
Adryan <adryanynfante@gmail.com>
2022-05-17 18:23:21 -0500

    se crea agregado cliente
    agregado cliente con sus values y entidades correspondiente

--- Domain/src/main/java/co/com/appventas/envio/cliente/Cliente.java
+++ Domain/src/main/java/co/com/appventas/envio/cliente/Cliente.java
@@ -1,4 +1,20 @@
 package co.com.appventas.envio.cliente;
 
-public class Cliente {
+import co.com.appventas.envio.cliente.values.Bono;
+import co.com.appventas.envio.cliente.values.ClienteId;
+import co.com.appventas.envio.cliente.values.DatosPersonalesCliente;
+import co.com.sofka.domain.generic.AggregateEvent;
+
+import java.util.Set;
+
+public class Cliente  extends AggregateEvent<ClienteId> {
+    protected DatosPersonalesCliente datosPersonales;
+    protected Bono bono;
+    protected Set<Devolucion> devolucion;
+    protected Set<Contacto> contacto;
+
+    public Cliente(ClienteId entityId) {
+        super(entityId);
+    }
+
 }
