d065aacd198b0e17fc7d96a1034add6303101cec
Adryan <adryanynfante@gmail.com>
2022-05-17 18:44:23 -0500

    primer evento agregado cliente
    cliente creado

--- Domain/src/main/java/co/com/appventas/envio/cliente/Cliente.java
+++ Domain/src/main/java/co/com/appventas/envio/cliente/Cliente.java
@@ -1,5 +1,6 @@
 package co.com.appventas.envio.cliente;
 
+import co.com.appventas.envio.cliente.events.ClienteCreado;
 import co.com.appventas.envio.cliente.values.Bono;
 import co.com.appventas.envio.cliente.values.ClienteId;
 import co.com.appventas.envio.cliente.values.DatosPersonalesCliente;
@@ -10,11 +11,14 @@ import java.util.Set;
 public class Cliente  extends AggregateEvent<ClienteId> {
     protected DatosPersonalesCliente datosPersonales;
     protected Bono bono;
-    protected Set<Devolucion> devolucion;
-    protected Set<Contacto> contacto;
+
 
     public Cliente(ClienteId entityId) {
         super(entityId);
     }
 
+    public Cliente(ClienteId entityId, DatosPersonalesCliente datosPersonales, Bono bono) {
+        super(entityId);
+        appendChange(new ClienteCreado(datosPersonales, bono)).apply();
+    }
 }
