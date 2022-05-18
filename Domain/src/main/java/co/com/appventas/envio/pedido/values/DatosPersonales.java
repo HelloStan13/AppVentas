4fd6503eae214daa62eae45ff94a3e1d7886627b
AngelaEspinosa <titagena@gmail.com>
2022-05-18 08:40:49 -0500

    se corrigen las validaciones del ValueObject DatosPersonales.

--- Domain/src/main/java/co/com/appventas/envio/pedido/values/DatosPersonales.java
+++ Domain/src/main/java/co/com/appventas/envio/pedido/values/DatosPersonales.java
@@ -2,15 +2,17 @@ package co.com.appventas.envio.pedido.values;
 
 import co.com.sofka.domain.generic.ValueObject;
 
+import java.util.Objects;
+
 public class DatosPersonales implements ValueObject<DatosPersonales.Props> {
     private final String NombreCompleto;
     private final String Telefono;
     private final String Correo;
 
     public DatosPersonales(String nombreCompleto, String telefono, String correo) {
-        NombreCompleto = nombreCompleto;
-        Telefono = telefono;
-        Correo = correo;
+        NombreCompleto = Objects.requireNonNull(nombreCompleto);
+        Telefono = Objects.requireNonNull(telefono);
+        Correo = Objects.requireNonNull(correo);
     }
 
     @Override
