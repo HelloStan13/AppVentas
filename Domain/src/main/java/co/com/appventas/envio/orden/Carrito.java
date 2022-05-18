5377a0b25c8398457686cb5db54509d3beffef0e
AngelaEspinosa <titagena@gmail.com>
2022-05-18 11:04:59 -0500

    Se crean los comportamientos al interior del agregado Carrito y de la entidad Factura.

--- Domain/src/main/java/co/com/appventas/envio/orden/Carrito.java
+++ Domain/src/main/java/co/com/appventas/envio/orden/Carrito.java
@@ -1,6 +1,8 @@
 package co.com.appventas.envio.orden;
 
 import co.com.appventas.envio.orden.events.CarritoCreado;
+import co.com.appventas.envio.orden.events.ProductoCreado;
+import co.com.appventas.envio.orden.events.ProductoEliminado;
 import co.com.appventas.envio.orden.values.*;
 import co.com.sofka.domain.generic.AggregateEvent;
 
@@ -21,6 +23,12 @@ public class Carrito extends AggregateEvent<CarritoId> {
         Objects.requireNonNull(precioProducto);
         Objects.requireNonNull(cantidad);
         appendChange( new ProductoCreado(carritoId,nombre,descripcion,precioProducto,cantidad)).apply();
+    }
+
+    public void eliminarProducto(CarritoId carritoId, ProductoId productoId){
+        Objects.requireNonNull(carritoId);
+        Objects.requireNonNull(productoId);
+        appendChange( new ProductoEliminado(carritoId,productoId)).apply();
     }
 
 }
--- Domain/src/main/java/co/com/appventas/envio/orden/events/ProductoCreado.java
+++ Domain/src/main/java/co/com/appventas/envio/orden/events/ProductoCreado.java
@@ -1,4 +1,4 @@
-package co.com.appventas.envio.orden;
+package co.com.appventas.envio.orden.events;
 
 import co.com.appventas.envio.orden.values.*;
 import co.com.sofka.domain.generic.DomainEvent;
