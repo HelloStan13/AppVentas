e5d052c0b702b8997bd1a055fd2db9431e167a44
Adryan <adryanynfante@gmail.com>
2022-05-18 09:38:00 -0500

    creacion de clases para comandos

---
+++ Domain/src/main/java/co/com/appventas/envio/cliente/commands/ActualizarMotivodeDevolucion.java
@@ -0,0 +1,7 @@
+package co.com.appventas.envio.cliente.commands;
+
+import co.com.sofka.domain.generic.Command;
+
+public class ActualizarMotivodeDevolucion extends Command {
+
+}
---
+++ Domain/src/main/java/co/com/appventas/envio/cliente/commands/EnviarPQR.java
@@ -0,0 +1,6 @@
+package co.com.appventas.envio.cliente.commands;
+
+import co.com.sofka.domain.generic.Command;
+
+public class EnviarPQR extends Command {
+}
