0619d2535eedcab01ff865d57c70c32e2b6cbc2b
Adryan <adryanynfante@gmail.com>
2022-05-17 21:07:27 -0500

    eventos de contactos ok

--- Domain/src/main/java/co/com/appventas/envio/cliente/Contacto.java
+++ Domain/src/main/java/co/com/appventas/envio/cliente/Contacto.java
@@ -1,6 +1,8 @@
 package co.com.appventas.envio.cliente;
 
 import co.com.appventas.envio.cliente.values.ContactoId;
+import co.com.appventas.envio.cliente.values.PQR;
+import co.com.appventas.envio.cliente.values.Whatsapp;
 import co.com.sofka.domain.generic.Entity;
 
 import java.util.Objects;
