830fc51a970fe9dbff656425a8cbd1b7a81896d3
AngelaEspinosa <titagena@gmail.com>
2022-05-17 19:09:18 -0500

    Se logra crear el comportamiento cambiarEntregado de la Entidad Estado, al igual que se genera el comportamiento junto con su evento de la creacion de la entidad Estado.

--- Domain/src/main/java/co/com/appventas/envio/pedido/Pedido.java
+++ Domain/src/main/java/co/com/appventas/envio/pedido/Pedido.java
@@ -1,17 +1,29 @@
 package co.com.appventas.envio.pedido;
 
+import co.com.appventas.envio.pedido.events.EstadoCreado;
 import co.com.appventas.envio.pedido.events.PedidoCreado;
 import co.com.appventas.envio.pedido.values.Aislamiento;
+import co.com.appventas.envio.pedido.values.Entregado;
 import co.com.appventas.envio.pedido.values.PedidoId;
+import co.com.appventas.envio.pedido.values.TiempoDeEntrega;
 import co.com.sofka.domain.generic.AggregateEvent;
+
+import java.util.Objects;
 
 public class Pedido extends AggregateEvent<PedidoId> {
     protected Estado estadoId;
     protected Repartidor repartidorId;
     protected Aislamiento aislamiento;
 
-    public Pedido(PedidoId entityId, Estado estadoId, Repartidor repartidorId, Aislamiento aislamiento) {
-        super(entityId);
+    public Pedido(PedidoId pedidoId, Estado estadoId, Repartidor repartidorId, Aislamiento aislamiento) {
+        super(pedidoId);
         appendChange(new PedidoCreado(estadoId,repartidorId,aislamiento)).apply();
+    }
+
+    public void agregarEstado(PedidoId pedidoId, TiempoDeEntrega tiempoDeEntrega, Entregado entregado){
+        Objects.requireNonNull(pedidoId);
+        Objects.requireNonNull(tiempoDeEntrega);
+        Objects.requireNonNull(entregado);
+        appendChange(new EstadoCreado(pedidoId,tiempoDeEntrega,entregado)).apply();
     }
 }
--- Domain/src/main/java/co/com/appventas/envio/pedido/events/EstadoCreado.java
+++ Domain/src/main/java/co/com/appventas/envio/pedido/events/EstadoCreado.java
@@ -1,10 +1,12 @@
-package co.com.appventas.envio.pedido;
+package co.com.appventas.envio.pedido.events;
 
+import co.com.appventas.envio.pedido.values.Entregado;
+import co.com.appventas.envio.pedido.values.TiempoDeEntrega;
 import co.com.appventas.envio.pedido.values.PedidoId;
 import co.com.sofka.domain.generic.DomainEvent;
 
 public class EstadoCreado extends DomainEvent {
     public EstadoCreado(PedidoId pedidoId, TiempoDeEntrega tiempoDeEntrega, Entregado entregado) {
-        super();
+        super("co.com.appventas.envio.EstadoCreado");
     }
 }
